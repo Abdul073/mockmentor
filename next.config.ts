1e937ed268c283fc609643f7f7541d1395af6345
Abdul gaffar <abdulgaffar7274@gmail.com>
2025-04-10 23:11:14 +0530

    fix error

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,9 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  experimental: {
+    optimizePackageImports: ["@next/font", "lightningcss"],
+  },
 };
 
 export default nextConfig;
