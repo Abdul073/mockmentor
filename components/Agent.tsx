b10fdb30f81ee5dfb044de094fc192059756fcaa
Abdul gaffar <abdulgaffar7274@gmail.com>
2025-04-10 17:30:16 +0530

    implement interview generation

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -16,6 +16,8 @@ enum CallStatus {
 const Agent = ({ userName }: AgentProps) => {
   const callStatus = CallStatus.FINISHED;
   const isSpeaking = true;
+  const messages = ["Whats your name", "My name is abdul, nice to meet you"];
+  const lastMessage = messages[messages.length - 1];
 
   return (
     <>
@@ -48,18 +50,36 @@ const Agent = ({ userName }: AgentProps) => {
         </div>
       </div>
 
+      {messages.length > 0 && (
+        <div className="transcript-border">
+          <div className="transcript">
+            <p
+              key={lastMessage}
+              className={cn(
+                "transition-opacity duration-500 opacity-0",
+                "animate-fadeIn opacity-100"
+              )}
+            >
+              {lastMessage}
+            </p>
+          </div>
+        </div>
+      )}
+
       <div className="w-full flex justify-center">
         {callStatus !== "ACTIVE" ? (
           <button className="relative btn-call">
             <span
               className={cn(
-                "absolute animate-spin rounded-full opacity-75",
+                "absolute animate-ping rounded-full opacity-75",
                 callStatus !== "CONNECTING" && "hidden"
               )}
-            >
+            />
+            <span>
               {callStatus === "INACTIVE" || callStatus === "FINISHED"
                 ? "Call"
-                : ". . ."} />
+                : ". . ."}
+            </span>
           </button>
         ) : (
           <button className="btn-disconnect">END</button>
